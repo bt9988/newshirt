7a2dc0db3ed1d9f053ee46884cb467f3b389e85f
bt9988 <reallyrealdesigns@gmail.com>
2026-06-26 22:23:34 -0700

    Update shirt_trends_monitor.py

--- shirt_trends_monitor.py
+++ shirt_trends_monitor.py
@@ -51,7 +51,7 @@ EXCLUDED_MATERIALS = {
 }
 
 EXCLUDED_BRANDS = {
-    'supreme', 'bape', , 'brandy', 'hollister', 'knicks', 'spurs', 'westwood', 'vietnamese', 'arsenall', 'conor', 'old navy', 'drake', 'markle', 'gucci', 'nike', 'adidas'
+    'supreme', 'bape', 'gucci', 'nike', 'adidas'
 }
 
 # --- Setup Logging ---
